734de3f24853a1d4fac3e09a967d972d029cbeae
ntaneja1985 <93190220+ntaneja1985@users.noreply.github.com>
2024-09-14 18:05:27 +0530

    Added code for undefined in Javascript
    Added code for undefined in Javascript

---
+++ JavascriptAdvancedCourse/undefinedvsNotDefined.js
@@ -0,0 +1,13 @@
+//Not defined vs Undefined in Javascript
+
+//Variables need memory assigned to them before they are used
+console.log(a) //undefined
+var a = "Nishant";
+console.log(a) //Nishant
+//Initially all variables in Javascript are undefined.
+
+console.log(b)
+
+var b = "4"
+
+//obj !== undefined || obj !== null
